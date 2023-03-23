40053f54bb29d22bba11e0e5687e22defce07eae
Md_Shafiul_Alam <shafiul.alam@bjitgroup.com>
2023-03-23 16:54:10 +0600

    Initial Commit

---
+++ NotePad-SwiftUI/NotePad_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotePad_SwiftUIApp.swift
+//  NotePad-SwiftUI
+//
+//  Created by ShafiulAlam-00058 on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct NotePad_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
